9151ceef5f8701fbab3e508acc0a22ca0eb4137d
Souvik Banerjee <banerjeesouvik136@gmail.com>
2025-09-03 14:32:36 +0000

    Basic Program

---
+++ Basics/ticket_price.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Your AGe : "))
+if(age <= 0 ):
+    print("Enter Valid Age ")
+elif(18 >= age > 8):
+    print("Your Ticket Price per head 20 rs")
+else:
+    print("Your Ticket Price per head 50 rs")
