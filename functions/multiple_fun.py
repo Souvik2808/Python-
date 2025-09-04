58340c0ba8b2fe38cf5aedcadf33d7213cb36b29
Souvik Banerjee <banerjeesouvik136@gmail.com>
2025-09-04 16:44:53 +0000

    Function

--- functions/multiple_fun.py
+++ functions/multiple_fun.py
@@ -11,9 +11,10 @@ def take_user_input():
 # Even or Odd number check : 
 
 def check_even_odd(num):
-    result "Even Number" if num%2==0 else "Odd Number"
-    print(f"{num} is {}")
-
+    if(num % 2 == 0):
+        print(f"{num} is Even Number")
+    else:
+        print(f"{num} is Odd Number")
 
 # Prime Number Check : 
 
@@ -31,5 +32,208 @@ def prime_check(num):
 
 # Armstrong Number Check : 
 
+def armstrong(num):
+    sum = 0
+    temp = num
+    while(temp > 0):
+        reminder = temp % 10
+        sum = sum + (reminder+reminder+reminder)
+        temp = temp // 10
  
+    if(sum == num ):
+        print(f"{num} is Armstrong ")
+    else:
+        print(f"{num} is not Armstrong")
+
+
+# Neon Number Check...
+
+def neon_number(num):
+    squre = num ** 2
+    sum_of_digit = 0
+    temp = squre
+    while (temp > 0):
+        reminder = temp % 10
+        sum_of_digit += reminder
+        temp //= 10
+
+    if (sum_of_digit == num):
+        print(f"{num} is a neon number")
+    else:
+        print(f"{num} is not a neon number")
+
+
+
+# Palindrome Number Check :
+
+def palindrome(num):
+    reverse = 0
+    temp = num
+    while(temp > 0):
+        reminder = num % 10
+        reverse = reverse * 10 + reminder
+        temp = temp // 10
+
+    if(reverse == num):
+        print(f"{num} is Palindrome ")
+    else:
+        print(f"{num} is not Palindrome")
+
+
+# krishnamurty Number Check :
+
+def krishnamurty(num):
+    f = 1
+    s = 0
+    temp = num 
+    while(temp > 0):
+        reminder = num % 10
+        for i in range(reminder):
+            f = f*i
+        s = s+f
+        temp = temp // 10
+        s = 0
+
+    if(num == s):
+        print(f"{num} is krishnamurty")
+    else:
+        print(f"{num} is not krishnamurty")
+
+
+# Perfect Number Check...
+
+
+def perfect_number(num):
+    temp = num
+    sum = 0
+    for i in range (1, num):
+        if (num % i == 0):
+            sum += i
+    if (sum == temp):
+        print(f"{num} is a perfect number")
+    else:
+        print(f"{num} is not a perfect number")
+
+def automorphic_number(num):
+    squre_number = num ** 2
+    if (num % 10 == squre_number % 10):
+        print(f"{num} is an automorphic number")
+    else:
+        print(f"{num} is not an automorphic number")
+
+
+# Harshed Number Check ...
+
+
+def harshed_number(num):
+    sum = 0
+    temp = num
+    while (temp > 0):
+        reminder = temp % 10
+        sum += reminder
+        temp //= 10
+    
+    if (num % sum == 0):
+        print(f"{num} is a harshad number")
+    else:
+        print(f"{num} is not a harshad number")
+
+# Strong Number Check ...
+
+
+def strong_number(num):
+    temp = num
+    sum = 0
+    while (temp > 0):
+        reminder = temp % 10
+        f = 1
+        for i in range (1, reminder + 1):
+            f *= i
+        sum += f
+        temp //= 10
+    if (num == sum):
+        print(f"{num} is a strong number")
+    else:
+        print(f"{num} is not a strong number")
+
+
+# Spy number Check ....
+
+
+def spy_number(num):
+    sum = 0
+    product = 1
+    temp = num
+
+    while (temp > 0):
+        reminder = temp % 10
+        sum += reminder
+        product *= reminder
+        temp //= 10
+    
+    if (sum == product):
+        print(f"{num} is a spy number")
+    else:
+        print(f"{num} is not a spy number")
+
+
+
+
+is_loop = True
+while(is_loop):
+    print("1. Check number is even or odd")    
+    print("2. Check number is Prime or not")    
+    print("3. Check number is Armstrong or not") 
+    print("4. Check number is Neon or not")   
+    print("5. Check number is Palindrome or not")
+    print("6. Check number is Krishnamurty or not")    
+    print("7. Check number is Perfect or not")    
+    print("8. Check number is Automorphic or not")    
+    print("9. Check number is Harshad or not")    
+    print("10. Check number is Strong or not")    
+    print("11. Check number is Spy or not")    
+
+
+    try:
+        print("\n")
+        ch = int(input("Enter Your Choice : "))
+    except Exception as e:
+        print("Invalid Choice : ", e)
+        exit()
+
+    num = take_user_input()
+    if(ch == 1):
+        check_even_odd(num)
+    elif(ch == 2):
+        prime_check(num)
+    elif(ch == 3):
+        armstrong(num)
+    elif(ch == 4):
+        neon_number(num)
+    elif(ch == 5):
+        palindrome(num)
+    elif(ch == 6):
+        krishnamurty(num)
+    elif(ch == 7):
+        perfect_number(num)
+    elif (ch == 8):
+        automorphic_number(num)
+    elif (ch == 9):
+        haesed_number(num)
+    elif (ch == 10):
+        strong_number(num)
+    elif (ch == 11):
+        spy_number(num)
+
+    else:
+        print("Invalid Choice")
+    print("\n-----------------------\n")
+    print("Do you want to continue (y/n): ")
+    ans = input()
+    if (ans.lower() == "y"):
+        pass
+    else:
+        is_loop = False
+
+    
 
